5d00df3ad68f818a5ad2530e57bb260e6c4a7beb
kumarshanu19 <er.kumarshanu@gmail.com>
2022-12-25 21:42:42 +0530

    New Update to remove home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
   ],
   imports: [
     BrowserModule,
