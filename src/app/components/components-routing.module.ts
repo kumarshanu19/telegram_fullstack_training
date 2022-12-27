e6cab9cb8623670081c87b8858fda3f7a589a07f
kumarshanu19 <er.kumarshanu@gmail.com>
2022-12-27 22:01:46 +0530

    Home Page
    3d day

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
@@ -9,12 +10,7 @@ const routes: Routes = [
     pathMatch: 'full'
   }, {
     path: 'home',
-    component: LayoutComponent,
-    children: [
-      {
-       path: ''
-     }
-   ]
+    component: LayoutComponent
   }
 ];
 
