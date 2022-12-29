b429d74ec316687f1beed5f24e7a1da8e037f7d3
kumarshanu19 <er.kumarshanu@gmail.com>
2022-12-29 13:04:33 +0530

    Home page update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  menu: Array<any> = [
+    {
+      _id: 1,
+      img: 'https://images.unsplash.com/photo-1500912708295-4cf8b060f381?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvZmZlZSUyMGRhcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      title: "Tasty And Healthy 1",
+      price: 20,
+      discount: 15
+    },
+    {
+      _id: 2,
+      img: 'https://images.unsplash.com/photo-1517154196073-d000ca11b42b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNvZmZlZSUyMGRhcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      title: "Tasty And Healthy 2",
+      price: 20,
+      discount: 15
+    },
+    {
+      _id: 3,
+      img: 'https://images.unsplash.com/photo-1500912708295-4cf8b060f381?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvZmZlZSUyMGRhcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      title: "Tasty And Healthy 3",
+      price: 20,
+      discount: 15
+    },
+    {
+      _id: 4,
+      img: 'https://images.unsplash.com/photo-1500912708295-4cf8b060f381?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvZmZlZSUyMGRhcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      title: "Tasty And Healthy 4",
+      price: 20,
+      discount: 15
+    },
+    {
+      _id: 5,
+      img: 'https://images.unsplash.com/photo-1500912708295-4cf8b060f381?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvZmZlZSUyMGRhcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      title: "Tasty And Healthy 5",
+      price: 20,
+      discount: 15
+    },
+    {
+      _id: 6,
+      img: 'https://images.unsplash.com/photo-1500912708295-4cf8b060f381?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNvZmZlZSUyMGRhcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      title: "Tasty And Healthy 6",
+      price: 20,
+      discount: 15
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
